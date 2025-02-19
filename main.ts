5db7bcb8206f86af312339510e88e40dd4d62a84
Luiz Guilherme Sales Munhoz <94804469+frpitu@users.noreply.github.com>
2025-02-19 20:01:05 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+/*
+
+8888888b.888888888888888888888     888
+888   Y88b 888      888    888     888
+888    888 888      888    888     888
+888   d88P 888      888    888     888
+8888888P"  888      888    888     888
+888        888      888    888     888
+888        888      888    Y88b. .d88P
+888      8888888    888     "Y88888P"
+
+
+__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __
+
+
+Discord: frpitu
+Twitter: @mechamopitu
+Github: https://github.com/frpitu
+
+*/
+
+/**
+ * Script básica de Hello World
+ */
+basic.showString('Olá mundo!');
